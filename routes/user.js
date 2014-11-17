784b38c0ffbf50f6571af8d58b2f759e0a4e6f1a
newxan <lundh123@gmail.com>
2014-11-17 10:00:23 +0100

    added so that user story now gives you the hit beacons name

--- routes/user.js
+++ routes/user.js
@@ -12,20 +12,35 @@ exports.list = function(req, res)
      console.log(user_query.sql);
   });
 }
-exports.story = function(req, res){
-    
-    var id = req.params.id;
-    
-    req.getConnection(function(err,connection){
-        var query = connection.query('SELECT * FROM report WHERE user=? ORDER BY timestamp ASC',[id],function(err,rows)
+exports.story = function(req, res)
+{
+  var async = require('async');
+  var id = req.params.id;
+  
+  req.getConnection(function(err,connection)
+  {
+    var query = connection.query('SELECT * FROM report WHERE user=? ORDER BY timestamp ASC',[id],function(err,rows)
+    {
+      if(err)
+          console.log("Error Selecting : %s ",err );
+      var totalReports = []
+      async.forEach(rows, function(report, callback) {
+        var query = connection.query('SELECT name FROM beacon WHERE id IN (SELECT beacon FROM hit WHERE report='+report.id+')',function(err,beacons)
         {
-            
-            if(err)
-                console.log("Error Selecting : %s ",err );
-     
-            res.render('show_user',{page_title:"User story",data:rows});
-         });
-         
-         console.log(query.sql);
-    }); 
+          if(err)
+            console.log("Error Selecting : %s ",err );
+          //console.log(beacons[0].name)
+          report.beacons = beacons;
+          totalReports.push(report)
+          callback();
+        });
+
+      }, function(err) {
+        if (err) return next(err);
+          console.log(totalReports);
+          res.render('show_user',{page_title:"User story",data:totalReports});
+      });
+    });
+    console.log(query.sql);
+  }); 
 };
